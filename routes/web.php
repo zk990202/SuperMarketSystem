da1ca82aea84989cb5c81714cc35fd8f8374541a
zhangkai <zhangkai@twt.edu.com>
2018-05-13 18:31:09 +0800

    Add auth middleware

--- routes/web.php
+++ routes/web.php
@@ -17,43 +17,45 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::group(['middleware' => 'RoleControlAdmin'], function (){
-    //查看商品信息
-    Route::get('/select-page/products', 'MainController@selectPageProducts');
-    Route::get('/select-page/sales', 'MainController@selectPageSales');
-    //查看销售信息
-    Route::post('/select/products', 'MainController@selectProducts');
-    Route::post('/select/sales', 'MainController@selectSales');
-});
+Route::group(['middleware' => 'auth'], function(){
+    Route::group(['middleware' => 'RoleControlAdmin'], function (){
+        //查看商品信息
+        Route::get('/select-page/products', 'MainController@selectPageProducts');
+        Route::get('/select-page/sales', 'MainController@selectPageSales');
+        //查看销售信息
+        Route::post('/select/products', 'MainController@selectProducts');
+        Route::post('/select/sales', 'MainController@selectSales');
+    });
 
 
-Route::group(['middleware' => 'RoleControlManagement'], function (){
-    //下架商品
-    Route::get('/drop-off-page', ['uses' => 'MainController@dropOffPage', 'as' => 'drop-off-page']);
-    Route::post('/drop-off', 'MainController@dropOff');
+    Route::group(['middleware' => 'RoleControlManagement'], function (){
+        //下架商品
+        Route::get('/drop-off-page', ['uses' => 'MainController@dropOffPage', 'as' => 'drop-off-page']);
+        Route::post('/drop-off', 'MainController@dropOff');
 
-    //统计销售情况
-    Route::get('/statistics/day', 'MainController@statisticsDay');
-    Route::get('/statistics/week', 'MainController@statisticsWeek');
-    Route::get('/statistics/month', 'MainController@statisticsMonth');
-    Route::get('/statistics/season', 'MainController@statisticsSeason');
-    Route::get('/statistics/year', 'MainController@statisticsYear');
-    //导出Excel
-    Route::post('/excel/export', 'MainController@excelExport');
-});
-
-
-Route::group(['middleware' => 'RoleControlPurchase'], function (){
-    //对缺货的商品进行进货
-    Route::get('/purchase-page', 'MainController@purchasePage');
-    Route::post('/purchase', 'MainController@purchase');
-});
+        //统计销售情况
+        Route::get('/statistics/day', 'MainController@statisticsDay');
+        Route::get('/statistics/week', 'MainController@statisticsWeek');
+        Route::get('/statistics/month', 'MainController@statisticsMonth');
+        Route::get('/statistics/season', 'MainController@statisticsSeason');
+        Route::get('/statistics/year', 'MainController@statisticsYear');
+        //导出Excel
+        Route::post('/excel/export', 'MainController@excelExport');
+    });
 
 
-Route::group(['middleware' => 'RoleControlCustomer'], function (){
-    //模拟销售
-    Route::get('/sale-page', 'MainController@salePage');
-    Route::post('/sale', 'MainController@sale');
+    Route::group(['middleware' => 'RoleControlPurchase'], function (){
+        //对缺货的商品进行进货
+        Route::get('/purchase-page', 'MainController@purchasePage');
+        Route::post('/purchase', 'MainController@purchase');
+    });
+
+
+    Route::group(['middleware' => 'RoleControlCustomer'], function (){
+        //模拟销售
+        Route::get('/sale-page', 'MainController@salePage');
+        Route::post('/sale', 'MainController@sale');
+    });
 });
 
 
